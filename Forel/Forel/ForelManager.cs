339eba89d5f36f7f0c332660b5a73416e18a1199
superviktor <victor.pryhidko@gmail.com>
2015-09-27 19:34:05 +0300

    need display elipse

---
+++ Forel/Forel/ForelManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forel
+{
+    public class ForelManager
+    {
+         public List<Point> points = new List<Point>();
+        public List<List<Point>> result = new List<List<Point>>();
+        public List<Point> centers = new List<Point>();
+        public double R = 1.65;
+
+        public void GetData()
+        {
+            String input = File.ReadAllText("inputFile.txt");
+            int i = 0;
+            int j = 0;
+            double[,] resultArray = new double[17, 2];
+            foreach (string row in input.Split('\n'))
+            {
+                j = 0;
+
+                foreach (string col in row.Trim().Split(','))
+                {
+                    resultArray[i, j] = Double.Parse(col, CultureInfo.InvariantCulture);
+                    j++;
+                }
+                i++;
+            }
+
+            for (int k = 0; k < resultArray.GetLength(0); k++)
+            {
+                for (int l = 0; l < resultArray.GetLength(1); l++)
+                {
+                    points.Add(new Point(resultArray[k, l], resultArray[k, l + 1]));
+                    break;
+                }
+            }
+        }
+
+        public void Cluster()
+        {
+            
+            while (points.Count > 0)
+            {
+                
+                Random rnd = new Random();
+                var index = rnd.Next(0, points.Count);
+                Point center = points[index];
+                Point newCenter = center;
+                //inside points 
+                List<Point> lst = new List<Point>();
+                while (center == newCenter)
+                {
+                    foreach (var p in points)
+                    {
+                        if (Math.Sqrt(Math.Pow((p.X - center.X), 2) + Math.Pow((p.Y - center.Y), 2)) < R)
+                        {
+                            lst.Add(p);
+                        }
+                    }
+                    //power center
+                    double powerX = 0;
+                    double powerY = 0;
+                    foreach (var p in lst)
+                    {
+                        powerX += p.X;
+                        powerY += p.Y;
+                    }
+                    double powerCenterX = powerX/lst.Count;
+                    double powerCenterY = powerY/lst.Count;
+                    newCenter = new Point(powerCenterX, powerCenterY);
+                }
+                result.Add(lst);
+                centers.Add(newCenter);
+                foreach (var p in lst)
+                {
+                    points.Remove(p);
+                }
+            }
+        }
+    }
+}
---
+++ Forel/ForelDisplaying/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Forel;
+
+namespace ForelDisplaying
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        ForelManager m = new ForelManager();
+        private void DisplayDefault_Click(object sender, RoutedEventArgs e)
+        {           
+            m.GetData();
+            foreach (var p in m.points)
+            {
+                Ellipse elipse = new Ellipse();
+                elipse.Fill = new SolidColorBrush(Colors.LightGray);
+                elipse.StrokeThickness = 1;
+                elipse.Stroke = Brushes.Black;
+                elipse.Width = 15;
+                elipse.Height = 15;
+                Canvas.SetTop(elipse,p.Y*25);
+                Canvas.SetLeft(elipse,p.X*25);
+                defaultCanvas.Children.Add(elipse);
+            }
+        }
+
+        private void Cluster_Click(object sender, RoutedEventArgs e)
+        {
+            m.Cluster();
+            foreach (var cluster in m.result)
+            {
+                
+                Color[] clrs = new Color[] { Colors.Red, Colors.SkyBlue, Colors.Orange, Colors.ForestGreen, Colors.Blue, Colors.DeepPink, Colors.Lime, Colors.BlueViolet, Colors.Aqua };              
+                var color = new SolidColorBrush(clrs[m.result.IndexOf(cluster)]);
+                foreach (var p in cluster)
+                {
+                    Ellipse elipse = new Ellipse();
+                    elipse.Fill = color;
+                    elipse.StrokeThickness = 1;
+                    elipse.Stroke = Brushes.Black;
+                    elipse.Width = 15;
+                    elipse.Height = 15;
+                    Canvas.SetTop(elipse, p.Y * 25);
+                    Canvas.SetLeft(elipse, p.X * 25);
+                    resultCanvas.Children.Add(elipse);
+                }                
+            }
+
+            Color[] backClrs = new Color[] { Colors.LightGoldenrodYellow, Colors.LightCyan, Colors.LightBlue, Colors.LightSteelBlue, Colors.LightYellow };              
+            foreach (var c in m.centers)
+            {
+                var color = new SolidColorBrush(backClrs[m.centers.IndexOf(c)]);
+                Ellipse elps = new Ellipse();
+                elps.Fill = color;
+                elps.Opacity = 0.4;
+                elps.StrokeThickness = 2;
+                elps.Stroke = Brushes.Black;               
+                Canvas.SetTop(elps, c.Y * 25);
+                Canvas.SetLeft(elps, c.X * 25);
+                elps.Width = 5;
+                elps.Height = 5;
+                resultCanvas.Children.Add(elps);
+            }
+
+        }
+    }
+}
